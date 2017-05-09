8d00180b8ee35d3f9e7be152d56385817a91356a
Tobias Kunze <rixx@cutebit.de>
2017-05-09 18:45:17 +0200

    Store data in SQLite (#22)
    Store data in SQLite rather than in shared memory segments for better compatability and speed.

--- asgi_ipc.py
+++ asgi_ipc.py
@@ -1,11 +1,13 @@
 from __future__ import unicode_literals
 
-import mmap
 import msgpack
+import os
 import random
 import six
+import sqlite3
 import string
-import struct
+import sys
+import tempfile
 import threading
 import time
 from asgiref.base_layer import BaseChannelLayer
@@ -42,15 +44,13 @@ class IPCChannelLayer(BaseChannelLayer):
         )
         self.thread_lock = threading.Lock()
         self.prefix = prefix
-        self.channel_store = MemoryDict(
-            "/%s-chan" % self.prefix,
-            size=channel_memory
-        )
+        temp_dir = tempfile.gettempdir()
+        file_name = prefix + '.sqlite'
+        connection = sqlite3.connect(os.path.join(temp_dir, file_name))
+        connection.text_factory = str
+        self.message_store = MessageTable(connection, prefix)
         # Set containing all groups to flush
-        self.group_store = MemoryDict(
-            "/%s-group" % self.prefix,
-            size=group_memory
-        )
+        self.group_store = GroupTable(connection, prefix)
 
     # --------
     # ASGI API
@@ -71,12 +71,16 @@ class IPCChannelLayer(BaseChannelLayer):
             channel = self.non_local_name(channel)
         # Write message into the correct message queue
         with self.thread_lock:
-            channel_list = self.channel_store.get(channel, [])
-            if len(channel_list) >= self.get_capacity(channel):
+            channel_size = self.message_store.get_message_count(channel)
+
+            if channel_size >= self.get_capacity(channel):
                 raise self.ChannelFull
             else:
-                channel_list.append([message, time.time() + self.expiry])
-                self.channel_store[channel] = channel_list
+                towrite = msgpack.packb(message, use_bin_type=True)
+                self.message_store.add_message(
+                    message=towrite, channel=channel,
+                    expiry=time.time() + self.expiry
+                )
 
     def receive(self, channels, block=False):
         if not channels:
@@ -89,15 +93,12 @@ class IPCChannelLayer(BaseChannelLayer):
         # Try to pop off all of the named channels
         with self.thread_lock:
             for channel in channels:
-                channel_list = self.channel_store.get(channel, [])
                 # Keep looping on the channel until
                 # we hit no messages or an unexpired one
                 while True:
                     try:
-                        # Popleft equivalent
-                        message, expires = channel_list[0]
-                        channel_list = channel_list[1:]
-                        self.channel_store[channel] = channel_list
+                        message, expires = self.message_store.pop_message(channel)
+                        message = msgpack.unpackb(message, encoding="utf8")
                         if expires <= time.time():
                             continue
                         # If there is a full channel name stored in the message, unpack it.
@@ -105,11 +106,8 @@ class IPCChannelLayer(BaseChannelLayer):
                             channel = message['__asgi_channel__']
                             del message['__asgi_channel__']
                         return channel, message
-                    except IndexError:
+                    except ValueError:
                         break
-                # If the channel is now empty, delete its key
-                if not channel_list and channel in self.channel_store:
-                    del self.channel_store[channel]
         return None, None
 
     def new_channel(self, pattern):
@@ -121,7 +119,7 @@ class IPCChannelLayer(BaseChannelLayer):
             new_name = pattern + random_string
             # To see if it's present we open the queue without O_CREAT
             with self.thread_lock:
-                if new_name not in self.channel_store:
+                if new_name not in self.message_store:
                     return new_name
                 else:
                     continue
@@ -136,9 +134,7 @@ class IPCChannelLayer(BaseChannelLayer):
         """
         assert self.valid_group_name(group), "Invalid group name"
         with self.thread_lock:
-            group_dict = self.group_store.get(group, {})
-            group_dict[channel] = time.time() + self.group_expiry
-            self.group_store[group] = group_dict
+            self.group_store.add_channel(group=group, channel=channel, expiry=time.time() + self.group_expiry)
 
     def group_discard(self, group, channel):
         """
@@ -147,27 +143,7 @@ class IPCChannelLayer(BaseChannelLayer):
         """
         assert self.valid_group_name(group), "Invalid group name"
         with self.thread_lock:
-            group_dict = self.group_store.get(group, {})
-            if channel in group_dict:
-                del group_dict[channel]
-                if not group_dict:
-                    del self.group_store[group]
-                else:
-                    self.group_store[group] = group_dict
-
-    def group_channels(self, group):
-        """
-        Returns all group channel names.
-        """
-        with self.thread_lock:
-            group_dict = self.group_store.get(group, {})
-        for channel, expires in list(group_dict.items()):
-            if expires <= time.time():
-                del group_dict[channel]
-                with self.thread_lock:
-                    self.group_store[group] = group_dict
-            else:
-                yield channel
+            self.group_store.discard_channel(group, channel)
 
     def send_group(self, group, message):
         """
@@ -180,6 +156,12 @@ class IPCChannelLayer(BaseChannelLayer):
             except self.ChannelFull:
                 pass
 
+    def group_channels(self, group):
+        result = [channel[0] for channel in self.group_store.get_current_channels(group)]
+        if sys.version_info[0] == 2:
+            result = [channel.decode() for channel in result]
+        return result
+
     # ---------------
     # Flush extension
     # ---------------
@@ -189,188 +171,113 @@ class IPCChannelLayer(BaseChannelLayer):
         Deletes all messages and groups.
         """
         with self.thread_lock:
-            self.channel_store.flush()
+            self.message_store.flush()
             self.group_store.flush()
 
     def __str__(self):
         return "%s(prefix=%s)" % (self.__class__.__name__, self.prefix)
 
 
-class MemoryDatastructure(object):
+class SqliteTable(object):
     """
-    Generic memory datastructure class; used for sets for flush tracking,
-    dicts for group membership, and lists for channels.
+    Generic sqlite datastructure class; used for dicts for group membership,
+    and lists for channels.
     """
-
-    # Maximum size of the datastructure. Try and override to not use up memory.
-    size = 1024 * 1024 * 5
-
     # How long to wait for the semaphore before declaring deadlock and flushing
     death_timeout = 2
 
-    # Datatype to store in here
-    datatype = dict
-
-    # Version signature - 8 bytes.
-    signature = None
-
-    def __init__(self, path, size=None):
-        if self.signature is None:
-            raise ValueError("No signature for this memory datastructure")
-        if size:
-            self.size = size
-        self.path = path
+    def __init__(self, connection, identifier):
+        self.connection = connection
+        self.identifier = '/' + identifier + self.table_name + '-sem'
         # TODO: Investigate having separate read and write locks to allow
         # concurrent reads.
         self.semaphore = posix_ipc.Semaphore(
-            self.path + "-sem",
+            self.identifier,
             flags=posix_ipc.O_CREAT,
             mode=0o660,
             initial_value=1,
         )
-        try:
-            self.shm = posix_ipc.SharedMemory(
-                self.path,
-                flags=posix_ipc.O_CREAT,
-                mode=0o660,
-                size=self.size,
-            )
-        except ValueError as e:
-            raise ValueError(
-                "Unable to allocate shared memory segment"
-                "(potentially out of memory).\n"
-                "Error was: %s" % e
-            )
-        self.mmap = mmap.mmap(self.shm.fd, self.size)
-
-    @classmethod
-    def exists(cls, path):
-        """
-        Returns True if this already exists.
-        """
-        # Open without O_CREAT so it doesn't make if not exist
-        try:
-            posix_ipc.SharedMemory(path)
-        except posix_ipc.ExistentialError:
-            return False
-        else:
-            return True
-
-    def _get_value(self):
-        # TODO: Look into version number for contents coupled with internal
-        # cache to avoid re-reading mmap for every access
-        try:
-            self.semaphore.acquire(self.death_timeout)
-        except posix_ipc.BusyError:
-            self._reset()
-            self.semaphore.acquire(0)
-        try:
-            # Seek to start of memory segment
-            self.mmap.seek(0)
-            # The first four bytes should be "ASGD", followed by four bytes
-            # of version (we're looking for 0001)
-            signature = self.mmap.read(8)
-            if signature != self.signature:
-                # Start fresh
-                return self.datatype()
-            else:
-                # There should then be four bytes of length
-                size = struct.unpack(b'!I', self.mmap.read(4))[0]
-                return msgpack.unpackb(self.mmap.read(size), encoding="utf8")
-        finally:
-            self.semaphore.release()
-
-    def _set_value(self, value):
-        assert isinstance(value, self.datatype)
-        try:
-            self.semaphore.acquire(self.death_timeout)
-        except posix_ipc.BusyError:
-            self._reset()
-            self.semaphore.acquire(0)
-        try:
-            self.mmap.seek(0)
-            self.mmap.write(self.signature)
-            towrite = msgpack.packb(value, use_bin_type=True)
-            self.mmap.write(struct.pack(b'!I', len(towrite)))
-            self.mmap.write(towrite)
-        finally:
-            self.semaphore.release()
+        connection.cursor().execute(self.table_structure)
+        connection.commit()
 
     def _reset(self):
         """
         Resets the semaphore if it's got stuck by a process that exited without
         releasing it.
         """
-        # Make the mmap empty enough that get will ignore it
-        self.mmap.seek(0)
-        self.mmap.write(b"\0\0\0\0\0\0\0\0")
         # Unlink and remake the semaphore
         self.semaphore.unlink()
         self.semaphore = posix_ipc.Semaphore(
-            self.path + "-sem",
+            self.identifier,
             flags=posix_ipc.O_CREX,
             mode=0o660,
             initial_value=1,
         )
 
-    def __del__(self):
-        """
-        Explicitly closes the shared memory area.
-
-        The semaphore is not closed as this is not threadsafe; closing it
-        prevents any other layers in the same process from seeing it.
-        """
-        self.mmap.close()
-        self.shm.close_fd()
-        self.shm.unlink()
-
     def flush(self):
-        self._set_value(self.datatype())
+        self.connection.cursor().execute('DELETE FROM {table_name}'.format(table_name=self.table_name))
+        self.connection.commit()
 
-    def __iter__(self):
-        return iter(self._get_value())
-
-    def __contains__(self, item):
-        return item in self._get_value()
-
-    def __getitem__(self, key):
-        return self._get_value()[key]
-
-    def __setitem__(self, key, value):
-        d = self._get_value()
-        d[key] = value
-        self._set_value(d)
-
-    def __delitem__(self, key):
-        d = self._get_value()
-        del d[key]
-        self._set_value(d)
-
-    def __len__(self):
-        return len(self._get_value())
+    def _execute(self, query, *args):
+        try:
+            self.semaphore.acquire(self.death_timeout)
+        except posix_ipc.BusyError:
+            self._reset()
+            self.semaphore.acquire(0)
+        try:
+            cursor = self.connection.cursor()
+            cursor.execute(query.format(table=self.table_name), args)
+            result = cursor.fetchall()
+        finally:
+            self.semaphore.release()
+        return result
 
 
-class MemoryDict(MemoryDatastructure):
-    """
-    Memory backed dict. Used for group membership.
-    """
+class MessageTable(SqliteTable):
+    table_name = 'messages'
+    table_structure = '''
+        CREATE TABLE IF NOT EXISTS messages
+        (id integer primary key, channel text, message text, expiry datetime)
+    '''
 
-    signature = b"ASGD0001"
+    def get_messages(self, channel):
+        return self._execute('SELECT message, expiry FROM {table} WHERE channel=?', channel) or (None, None)
 
-    def items(self):
-        return self._get_value().items()
+    def get_message_count(self, channel):
+        return self._execute('SELECT COUNT(*) FROM {table} WHERE channel=?', channel)[0][0]
 
-    def keys(self):
-        return self._get_value().keys()
+    def add_message(self, message, expiry, channel):
+        self._execute('INSERT INTO {table} (channel, message, expiry) VALUES (?,?,?)', channel, message, expiry)
 
-    def values(self):
-        return self._get_value().values()
+    def pop_message(self, channel):
+        result = self._execute('SELECT id, message, expiry FROM {table} WHERE channel=? LIMIT 1', channel)
+        if not result:
+            raise ValueError('No message in channel')
+        result = result[0]
+        self._execute('DELETE FROM {table} WHERE id=?', result[0])
+        return result[1], result[2]
 
-    def get(self, key, default):
-        return self._get_value().get(key, default)
+    def __contains__(self, value):
+        result = self._execute('SELECT COUNT(*) FROM {table} WHERE channel=?', value)[0][0]
+        return bool(result)
 
-    def discard(self, item):
-        value = self._get_value()
-        if item in value:
-            del value[item]
-        self._set_value(value)
+
+class GroupTable(SqliteTable):
+    table_name = 'groups'
+    table_structure = '''
+        CREATE TABLE IF NOT EXISTS groups
+        (channel text, group_name text, expiry datetime)
+    '''
+
+    def add_channel(self, group, channel, expiry):
+        self._execute('INSERT INTO {table} (channel, group_name, expiry) VALUES (?,?,?)', channel, group, expiry)
+
+    def discard_channel(self, group, channel):
+        self._execute('DELETE FROM {table} WHERE group_name=? AND channel=?', group, channel)
+
+    def _cleanup(self, group):
+        self._execute('DELETE FROM {table} WHERE group_name=? AND expiry<=?', group, time.time())
+
+    def get_current_channels(self, group):
+        self._cleanup(group)
+        return self._execute('SELECT DISTINCT channel FROM {table} WHERE group_name=?', group)
--- tests/test_asgi_ipc.py
+++ tests/test_asgi_ipc.py
@@ -1,7 +1,6 @@
 from __future__ import unicode_literals
 
-import unittest
-from asgi_ipc import IPCChannelLayer, MemoryDict
+from asgi_ipc import IPCChannelLayer
 from asgiref.conformance import ConformanceTestCase
 
 
@@ -11,25 +10,3 @@ class IPCLayerTests(ConformanceTestCase):
     channel_layer = IPCChannelLayer(expiry=1, group_expiry=2, capacity=5)
     expiry_delay = 1.1
     capacity_limit = 5
-
-
-# MemoryDict unit tests
-class MemoryDictTests(unittest.TestCase):
-
-    def setUp(self):
-        self.instance = MemoryDict("/test-md")
-        self.instance.flush()
-
-    def test_item_access(self):
-        # Make sure the key is not there to start
-        with self.assertRaises(KeyError):
-            self.instance["test"]
-        # Set it and check it twice
-        self.instance["test"] = "foo"
-        self.assertEqual(self.instance["test"], "foo")
-        self.instance["test"] = "bar"
-        self.assertEqual(self.instance["test"], "bar")
-        # Delete it and make sure it's gone
-        del self.instance["test"]
-        with self.assertRaises(KeyError):
-            self.instance["test"]
